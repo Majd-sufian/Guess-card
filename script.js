5ba424a46f4c44f297adf7569ac8da4649a1fa88
Majd Sufian <majdnewsufian@gmail.com>
2020-04-18 21:57:45 +0200

    convert the game from pick a number ro pick a card'

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 let correctNumber = getRandomNumber()
 console.log(correctNumber + "one")
 
+function getRandomNumber(){
+  var randomNumber = Math.round(Math.random() * 10)
+  return randomNumber
+}
+
+let images = {
+  1: 'img/1 d.png',
+  2: 'img/2 d.png',
+  3: 'img/3 d.png',
+  4: 'img/4 d.png',
+  5: 'img/5 d.png',
+  6: 'img/6 d.png',
+  7: 'img/7 d.png',
+  8: 'img/8 d.png',
+  9: 'img/9 d.png',
+  10: 'img/10 d.png',
+}
+
 let guesses = []
-console.log(guesses)
 
 window.onload = function() {
     document.getElementById("number-submit").addEventListener("click", playGame);
     document.getElementById("restart-game").addEventListener("click", initGame)
 }
 
-
 function playGame(){
-  // 1
   var history = document.getElementById('history');
   let numberGuess = document.getElementById('number-guess').value;
-  console.log(numberGuess)
-  // 2
   history.innerHTML = numberGuess
   displayResult(numberGuess)
   saveGuessHistory(numberGuess)
@@ -24,6 +37,7 @@ function playGame(){
 
 function displayResult(numberGuess){
   if (numberGuess == correctNumber) {
+    showImage()
     return(showYouWon())
   }
   else if (numberGuess > correctNumber) {
@@ -34,25 +48,27 @@ function displayResult(numberGuess){
   }
 }
 
+function showImage () {
+  var changeImage = `<img src="img/${correctNumber} d.png" alt="card"/>`
+  document.getElementById("guess-card-1").innerHTML = changeImage
+  document.getElementById("guess-card-2").innerHTML = changeImage
+}
 
 function initGame(){
   resetResultContent()
 }
 
 function resetResultContent(){
+  let restImage = '<img src="img/back d.png" alt="card"/>'
   document.getElementById("result").innerHTML = "";
   document.getElementById("history").innerHTML = "";
-
-}
-
-function getRandomNumber(){
-  var randomNumber = Math.round(Math.random() * 100)
-  return randomNumber
+  document.getElementById("guess-card-1").innerHTML = restImage
+  document.getElementById("guess-card-2").innerHTML = restImage
 }
 
 function saveGuessHistory(guess) {
   guesses.push(guess)
-  console.log(guesses)
+  let times = guess.length
 }
 
 function displayHistory() {
@@ -60,14 +76,14 @@ function displayHistory() {
   let index = 0
   let list = "<ul class='list-group'>";
   // *CODE GOES BELOW HERE *
+
   while(index < guesses.length){
     check = guesses[index] == correctNumber ? 'correct' : 'wrong'
   list += `<li class='list-group-item'>You guessed ${guesses[index]} - ${ check} </li>`
   index+=1
   }
-  list += '</ul>'
+  
   let gameTimes = `you tried ${guesses.length} times`
-  // 5
   document.getElementById("history").innerHTML = list;
   document.getElementById("times").innerHTML = gameTimes;
 }
